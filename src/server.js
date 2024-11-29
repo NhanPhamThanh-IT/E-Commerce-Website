3a5668455772973ef024843fd1b7313f87ddc6ed
nnnnhu22@clc.fitus.edu.vn <144573999+ngocnhu100@users.noreply.github.com>
2024-11-29 09:25:46 +0700

    add server.js and package.json

---
+++ src/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const session = require('express-session');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//config req.body
+app.use(express.json()) // for json
+app.use(express.urlencoded({ extended: true })) // for form data
+
+// config session management
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+app.use(cookieParser());
+
+// Route declarations
+
+// Connect to database and start server
+(async () => {
+    try {
+        await connection();
+
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log(">>> Error connect to DB: ", error)
+    }
+})()
