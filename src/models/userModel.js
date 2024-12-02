d2b7e9564428bf6c69991b590d0fa2a6d345bab1
nnnnhu22@clc.fitus.edu.vn <144573999+ngocnhu100@users.noreply.github.com>
2024-12-02 13:53:57 +0700

    add user model

---
+++ src/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const cartItemSchema = new mongoose.Schema({
+    productID: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, required: true, min: 1 },
+});
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true, sparse: true },
+    password: {
+        type: String,
+        required: function () {
+            return this.loginMethod === 'email';
+        },
+    },    
+    role: { type: String, enum: ['admin', 'user'], default: 'user' },
+    googleId: { type: String, default: null },
+    facebookId: { type: String, default: null },
+    loginMethod: { type: String, enum: ['email', 'google', 'facebook'], required: true, default: 'email' },
+    emailToken: { type: String, default: null  },
+    isVerified: { type: Boolean, default: false },
+    cart: [cartItemSchema],
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
