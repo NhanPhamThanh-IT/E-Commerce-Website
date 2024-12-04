c20d3d6034ebed1deef4f9ba19d2290d405be007
nnnnhu22@clc.fitus.edu.vn <144573999+ngocnhu100@users.noreply.github.com>
2024-12-04 11:25:29 +0700

    add fb + google routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+require('dotenv').config();
 const userController = require('../controllers/userController');
-const JwtAuth = require('../middlewares/jwtAuth');
+const Auth = require('../middlewares/Auth');
 
 router.post('/login', userController.handleLogin);
-
-router.use(JwtAuth.attachJwtToken);
-router.use(JwtAuth.passportAuth);
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
+router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }))
+router.get(
+    '/google/callback',
+    Auth.googleAuth,
+    (req, res, next) => {
+        req.login(req.user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/user/profile');
+        });
+    }
+)
+router.get(
+    '/facebook/callback',
+    Auth.facebookAuth,
+    (req, res, next) => {
+        req.login(req.user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/user/profile');
+        });
+    }
+)
+router.use(Auth.attachJwtToken);
+router.use(Auth.passportAuth);
 // router.get('/user/profile', userController.getUserByID);
+router.get('/profile',  (req, res) => {
+    console.log('req.user >>', req.user);
+    res.status(200).json("hi " + req.user.name);
+});
 router.post('/logout', userController.handleLogout);
 
 module.exports = router;
