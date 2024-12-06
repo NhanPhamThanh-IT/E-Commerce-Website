0856fd14e4833736b6df62332cf698f9efa3ec71
lnpnam22 <lnpnam22@clc.fitus.edu.vn>
2024-12-06 15:07:49 +0700

    update profile Controller

--- src_admin/app/controllers/profileController.js
+++ src_admin/app/controllers/profileController.js
@@ -1,8 +1,19 @@
-const userModel = require('../models/profileModel');
+const profileModel = require('../models/profileModel');
 
 exports.index = (req, res) => {
     const data = {
-        users: userModel
+        user: profileModel,
     };
-    res.render('users/index', data);
+    res.render('profile/index', data);
+};
+
+exports.edit = (req, res) => {
+    const new_name = req.body.name;
+    const new_email = req.body.email;
+    const profile = profileModel;
+
+    profile['name'] = new_name;
+    profile['email'] = new_email;
+
+    res.redirect('/admin/profile');
 };
