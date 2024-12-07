aadad315472917415cb33e3a3ae5b8ae41d12c7a
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-07 14:58:03 +0700

    Update dashboard controller for role admin

--- src_admin/app/controllers/dashboardController.js
+++ src_admin/app/controllers/dashboardController.js
@@ -1,10 +1,23 @@
-exports.index = (req, res) => {
-    const fakeData = {
-        users: 250,
-        products: 500,
-        orders: 150,
-        
-    };
+const User = require('../models/userModel');
+const Product = require('../models/productModel');
+const Order = require('../models/orderModel');
 
-    res.render('dashboard/index', fakeData);
+exports.index = async (req, res) => {
+    try {
+        const [usersCount, productsCount] = await Promise.all([
+            User.countDocuments(),
+            Product.countDocuments(),
+        ]);
+        const ordersCount = 150;
+        const fakeData = {
+            users: usersCount,
+            products: productsCount,
+            orders: ordersCount,
+        };
+        res.render('dashboard/index', fakeData);
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).send('Internal Server Error');
+    }
 };
+
