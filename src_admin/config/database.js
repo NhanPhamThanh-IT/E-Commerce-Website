21655caf1bf9c978a47a8f81e9747101122196e7
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-07 11:20:23 +0700

    Update config

--- src_admin/config/database.js
+++ src_admin/config/database.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect('mongodb+srv://abc123:test1234@test.4vzny.mongodb.net/hoi-dan-it-tutorial', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB Connected...');
+        console.log('MongoDB connected');
     } catch (err) {
-        console.error(err.message);
+        console.error('MongoDB connection error:', err);
         process.exit(1);
     }
 };
