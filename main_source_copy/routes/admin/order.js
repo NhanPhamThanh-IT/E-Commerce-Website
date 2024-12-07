bff4550f0c94bbcee4f37f9192153c12efbb5dfb
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-07 21:44:04 +0700

    Update source backup

--- main_source_copy/routes/admin/order.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../../controllers/admin/orderController');
-
-router.get('/', orderController.index);
-
-module.exports = router;
--- main_source_copy/routes/admin/user.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../../controllers/admin/userController');
-
-router.get('/', userController.index);
-
-module.exports = router;
