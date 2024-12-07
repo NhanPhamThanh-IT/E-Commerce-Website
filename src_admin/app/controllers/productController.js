5ac784b9a358183d67223dc13c33a7143600c7d1
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-07 14:08:11 +0700

    Update product controller: fix getProducts function

--- src_admin/app/controllers/productController.js
+++ src_admin/app/controllers/productController.js
@@ -1,16 +1,25 @@
 const Product = require('../models/productModel');
 
 exports.getProducts = async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = 6;
-    const skip = (page - 1) * limit;
-    const totalProducts = Product.length;
-    const products = Product.slice(skip, skip + limit);
-    res.render('products/index', {
-        products,
-        currentPage: page,
-        totalPages: Math.ceil(totalProducts / limit),
-    });
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = 6;
+        const skip = (page - 1) * limit;
+        const totalProducts = await Product.countDocuments();
+        const products = await Product.find()
+            .lean()
+            .skip(skip)
+            .limit(limit)
+            .sort({ createdAt: -1 });
+        res.render('products/index', {
+            products,
+            currentPage: page,
+            totalPages: Math.ceil(totalProducts / limit),
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 exports.getProductDetails = async (req, res) => {
