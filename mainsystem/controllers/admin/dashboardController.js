416160d416f68ac95f901bc4168bee4a22de8ca3
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-09 19:35:22 +0700

    Update dashboard for role admin

--- mainsystem/controllers/admin/dashboardController.js
+++ mainsystem/controllers/admin/dashboardController.js
@@ -4,17 +4,22 @@ const Order = require('../../models/orderModel');
 
 exports.index = async (req, res) => {
     try {
-        const [usersCount, productsCount] = await Promise.all([
-            User.countDocuments(),
+        const personalInfo = req.user.toObject();
+        const [usersCount, productsCount, verifiedCount] = await Promise.all([
+            User.countDocuments({ role: 'user' }),
             Product.countDocuments(),
+            User.countDocuments({ isVerified: true }),
         ]);
         const ordersCount = 150;
-        const fakeData = {
+        const data = {
             users: usersCount,
             products: productsCount,
             orders: ordersCount,
+            verified: verifiedCount,
+            unverified: usersCount - verifiedCount,
+            personalInfo,
         };
-        res.render('admin/dashboard/index', fakeData);
+        res.render('admin/dashboard/index', data);
     } catch (err) {
         console.error('Error fetching data:', err);
         res.status(500).send('Internal Server Error');
