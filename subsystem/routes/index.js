2d1074f61b62cf478b8b5bdabac04ee0f34d8491
nnnnhu22@clc.fitus.edu.vn <144573999+ngocnhu100@users.noreply.github.com>
2024-12-12 22:39:01 +0700

    add auth middleware

--- subsystem/routes/index.js
+++ subsystem/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 const transactionController = require('../controllers/transactionController');
+const paymentHistoryController = require('../controllers/paymentHistoryController');
 
 router.get('/transaction/:id', transactionController.index)
+router.post('/process-order', paymentHistoryController.index);
 
 module.exports = router;
