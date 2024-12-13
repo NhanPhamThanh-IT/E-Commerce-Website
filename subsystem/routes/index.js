6a7274847f45cc174c18fa51f29e85857600b9b8
nnnnhu22@clc.fitus.edu.vn <144573999+ngocnhu100@users.noreply.github.com>
2024-12-13 12:44:52 +0700

    add account information page

--- subsystem/routes/index.js
+++ subsystem/routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router();
 require('dotenv').config();
 const transactionController = require('../controllers/transactionController');
 const paymentHistoryController = require('../controllers/paymentHistoryController');
+const accountController = require('../controllers/accountController');
 
-router.get('/transaction/:id', transactionController.index)
+router.get('/transaction/:id', transactionController.index);
 router.post('/process-order', paymentHistoryController.index);
+router.get('/account', accountController.index);
+router.post('/add-money', accountController.addMoney);
 
 module.exports = router;
