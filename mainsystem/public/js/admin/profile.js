e1224ea45c990c0a967c06e9cdcfacb2d80ee15f
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-13 23:51:51 +0700

    Update new frontend following new theme

--- mainsystem/public/js/admin/profile.js
+++ mainsystem/public/js/admin/profile.js
@@ -1,8 +1,15 @@
 const editProfileButton = document.getElementById('editProfileButton');
 const editProfileModal = document.getElementById('editProfileModal');
 const closeModalButton = document.getElementById('closeModalButton');
+const loadingOverlay = document.getElementById('loadingOverlay');
 
 editProfileButton.addEventListener('click', () => {
+    loadingOverlay.classList.remove('hidden');
+    setTimeout(() => {
+        loadingOverlay.classList.remove('opacity-0');
+        loadingOverlay.classList.add('opacity-100');
+        loadingOverlay.classList.add('pointer-events-auto');
+    }, 10);
     editProfileModal.classList.remove('hidden');
     setTimeout(() => {
         editProfileModal.classList.remove('opacity-0');
@@ -11,11 +18,14 @@ editProfileButton.addEventListener('click', () => {
     }, 10);
 });
 
-closeModalButton.addEventListener('click', () => {
+closeModalButton.addEventListener('click', (event) => {
+    event.preventDefault();
     editProfileModal.classList.remove('opacity-100');
     editProfileModal.classList.add('opacity-0');
-    setTimeout(() => {
-        editProfileModal.classList.add('hidden');
-        editProfileModal.classList.remove('pointer-events-auto');
-    }, 300);
+    editProfileModal.classList.add('hidden');
+    editProfileModal.classList.remove('pointer-events-auto');
+    loadingOverlay.classList.remove('opacity-100');
+    loadingOverlay.classList.add('opacity-0');
+    loadingOverlay.classList.add('hidden');
+    loadingOverlay.classList.remove('pointer-events-auto');
 });
