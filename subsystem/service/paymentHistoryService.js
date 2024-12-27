5ac0361cbda15def349a36461f9cba4acb913747
NhanPhamThanh-IT <ptnhanit230104@gmail.com>
2024-12-27 12:58:49 +0700

    Update account frontend for subsystem

--- subsystem/service/paymentHistoryService.js
+++ subsystem/service/paymentHistoryService.js
@@ -2,7 +2,12 @@ const PaymentHistory = require('../../mainsystem/models/paymentHistoryModel');
 
 class paymentHistoryService {
     static async getById(id) {
-        return await PaymentHistory.find({ user_id: id }).lean()
+        const data = await PaymentHistory.aggregate([
+            { $match: { user_id: id } },
+            { $project: { order_id: 1, date: 1, total_payment: { $toDecimal: "$total_payment" } } },
+            { $addFields: { total_payment: { $round: ["$total_payment", 2] } } }
+        ]);
+        return data;
     }
 }
 
